abe60cf4dfec6e30ee6c9afaafb849564564ed89
yllwpan <tiffany.dharsono@binus.ac.id>
2022-01-12 09:25:51 +0700

    update

--- Main/DropdownAnswer.js
+++ Main/DropdownAnswer.js
@@ -1,30 +1,41 @@
 
 export default class Dropdown {
-    constructor(item, answers) {
+    constructor(item, answers, icon) {
 
         this.isOpen = item.classList.contains('hidden')
         this.answers = answers
         
         if (this.isOpen) {
-            this.setDropDown(item)
+            this.setDropDown(item, icon)
         } else {
-            this.disposeDropDown(item)
+            this.disposeDropDown(item, icon)
         }
     }
 
-    setDropDown(item) {
-        this.closeOpenTab()
+    setDropDown(item, icon) {
+        this.closeOpenTab(icon)
         item.classList.remove('hidden')
+        icon.style.transform = `rotate(90deg)`
+
+        window.setTimeout(function(){
+            item.style.opacity = 1;
+        },80);
     }
 
-    disposeDropDown(item) {
+    disposeDropDown(item, icon) {
         item.classList.add('hidden')
+        item.style.opacity = 0;
+        icon.style.transform = `rotate(0deg)`
     }
 
     closeOpenTab() {
         for (let answer of this.answers) {
             if (!answer.classList.contains('hidden')) {
                 answer.classList.add('hidden')
+                answer.style.opacity = 0
+                const container = answer.closest('.question-container')
+                const childContainer = container.children[0]
+                const icon = childContainer.children[0]
             }
         }
     }
